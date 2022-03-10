7056257ea70f66bca62fbd635a3df2000a106c42
IsaacOus <iouslimane@myges.fr>
2022-03-10 16:42:36 +0100

    [ADD] Get By Mail

--- src/controllers/user/get.account.mail.controller.ts
+++ src/controllers/user/get.account.mail.controller.ts
@@ -5,20 +5,21 @@ import {isNullOrUndefined} from "util";
 
 export async function getAccountMailController(request: Request, response: Response){
 
-    const mail = request.params.mail;
+    const mail = request.body.mail;
 
     const accountRepo = getManager().getRepository(Account);
 
     if(isNullOrUndefined(mail))
     {
-        const account = await accountRepo.createQueryBuilder()
-            .select("*")
-            .from(Account,"account")
-            .where("account.mail = :mail",mail)
-            .getOne();
-
-
-        response.send(account);
-        response.status(200).end();
+        response.status(400).end();
     }
+    const account = await accountRepo.find({
+        where: [
+            { mail: mail },
+        ],
+    });
+
+
+    response.send(account);
+    response.status(200).end();
 }
